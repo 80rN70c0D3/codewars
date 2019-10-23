67b80dfeea205d5f09358b0cb8ac3c046042a332
80rN70c0D3 <80rN70c0D3@protonmail.com>
2019-10-23 22:44:25 +0200

    update

--- categorize_new_member.js
+++ categorize_new_member.js
@@ -21,6 +21,26 @@ Example Output
 
 */
 
+//First attempt
+
 function openOrSenior(data){
-  // ...
+  let result = [];
+  for (var i = 0; i < data.length; i++) {
+    if (data[i][0] >= 55 && data[i][1] > 7) {
+      result.push('Senior');
+    }else{
+      result.push('Open');
+    }
+  }return result;
 }
+
+//Best Practices - by jhairau
+
+// Destructuring: [age, handicap] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+// Arrow Functions: () => {} https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+function openOrSenior(data){
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
+
+//end
