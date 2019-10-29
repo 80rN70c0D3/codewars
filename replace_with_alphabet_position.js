e9a1e8e172b1eebe9b4b9bbe44ebf2f77b96e547
80rN70c0D3 <80rN70c0D3@protonmail.com>
2019-10-29 21:08:56 +0100

    update

--- replace_with_alphabet_position.js
+++ replace_with_alphabet_position.js
@@ -16,7 +16,7 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 
 //First attempt
 
-alphabetPosition(text) {
+function alphabetPosition(text) {
   let result = [];
   let alphabetList = ['a', 'b', 'c' ,'d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   let splitText = text.split('');
