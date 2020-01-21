ca0bb9762eebc126a94359e7fc1c3289eb346009
80rN70c0D3 <80rN70c0D3@protonmail.com>
2020-01-21 21:55:46 +0100

    update

---
+++ decode_the_morse_code.js
@@ -0,0 +1,96 @@
+//Part of Series 1/3
+//decodeMorse('.... . -.--   .--- ..- -.. .')
+//should return "HEY JUDE"
+
+decodeMorse = function(str) {
+            
+        const morseCode = {
+        '-.-.--': '!',
+        '.-..-.': '"',
+        '...-..-': '$',
+        '.-...': '&',
+        '.----.': '\'',
+        '-.--.': '(',
+        '-.--.-': ')',
+        '.-.-.': '+',
+        '--..--': ',',
+        '-....-': '-',
+        '.-.-.-': '.',
+        '-..-.': '/',
+        '-----': '0',
+        '.----': '1',
+        '..---': '2',
+        '...--': '3',
+        '....-': '4',
+        '.....': '5',
+        '-....': '6',
+        '--...': '7',
+        '---..': '8',
+        '----.': '9',
+        '---...': ':',
+        '-.-.-.': ';',
+        '-...-': '=',
+        '..--..': '?',
+        '.--.-.': '@',
+        '.-': 'A',
+        '-...': 'B',
+        '-.-.': 'C',
+        '-..': 'D',
+        '.': 'E',
+        '..-.': 'F',
+        '--.': 'G',
+        '....': 'H',
+        '..': 'I',
+        '.---': 'J',
+        '-.-': 'K',
+        '.-..': 'L',
+        '--': 'M',
+        '-.': 'N',
+        '---': 'O',
+        '.--.': 'P',
+        '--.-': 'Q',
+        '.-.': 'R',
+        '...': 'S',
+        '-': 'T',
+        '..-': 'U',
+        '...-': 'V',
+        '.--': 'W',
+        '-..-': 'X',
+        '-.--': 'Y',
+        '--..': 'Z',
+        '..--.-': '_',
+        '...---...': 'SOS'
+        };
+
+        var decodeLetters = function(str) {
+        var result = '';
+        var splitLetters = str.split(' ');
+        for (var i = 0; i < splitLetters.length; i++) {
+            for (var key in morseCode) {
+                if (splitLetters[i] === key) {
+                    result += morseCode[key];
+                }
+            }
+        }return result;
+    }
+
+    var splitWords = str.split('  ');
+    var finalResult = '';
+    for (var j = 0; j < splitWords.length; j++) {
+        var decodeAll = decodeLetters(splitWords[j]);
+        finalResult += decodeAll + ' ';
+    } return finalResult.toUpperCase().trim();
+}
+
+
+//Most upvoted solution 
+
+decodeMorse = function(morseCode){
+    function decodeMorseLetter(letter) {
+      return MORSE_CODE[letter];
+    }
+    function decodeMorseWord(word) {
+      return word.split(' ').map(decodeMorseLetter).join('');
+    }
+    return morseCode.trim().split('   ').map(decodeMorseWord).join(' ');
+  }
